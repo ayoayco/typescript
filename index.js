476de6a7ce7be522788c9ee9afe1a430aa8142b7
Ayo Ayco <mail@ayoayco.com>
2016-03-12 01:53:31 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var Student = (function () {
+    function Student(firstname, middleinitial, lastname) {
+        this.firstname = firstname;
+        this.middleinitial = middleinitial;
+        this.lastname = lastname;
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+    return Student;
+})();
+
+/*interface Person {
+firstname: string;
+lastname: string;
+}*/
+function greeter(person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
